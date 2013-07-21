a11cced9e7799a4f34b063217e7ecdd0ebddc266
Jed Hunsaker <jed.hunsaker@gmail.com>
2013-07-20 20:09:18 -0700

    Finish trailing whitespace spec

--- test/lib/rules/trim_trailing_whitespace.js
+++ test/lib/rules/trim_trailing_whitespace.js
@@ -7,39 +7,87 @@ describe('trim_trailing_whitespace rule', function() {
         reporter.reset();
     });
 
+    it('infers "true" setting when no trailing whitespace is found', function() {
+        var result;
+        result = rule.infer('foo\n');
+        expect(result).to.be.true;
+        result = rule.infer('foo\r\n');
+        expect(result).to.be.true;
+        result = rule.infer('\r');
+        expect(result).to.be.true;
+    });
+
+    it('infers "false" setting when trailing whitespace is found', function() {
+        var result;
+        result = rule.infer('foo \n');
+        expect(result).to.be.false;
+        result = rule.infer('foo	 \r\n');
+        expect(result).to.be.false;
+        result = rule.infer('	 	\r');
+        expect(result).to.be.false;
+    });
+
     describe('true setting', function() {
 
-        it('reports trailing whitespace', function() {
-            rule.check(context, true, 'foo \n');
-            rule.check(context, true, 'foo	 \r\n');
-            rule.check(context, true, '	 	\r');
-            expect(reporter).to.have.been.calledThrice;
-            expect(reporter.alwaysCalledWithExactly('Trailing whitespace found.')).to.be.ok;
+        describe('check command', function() {
+
+            it('reports trailing whitespace', function() {
+                rule.check(context, true, 'foo \n');
+                rule.check(context, true, 'foo	 \r\n');
+                rule.check(context, true, '	 	\r');
+                expect(reporter).to.have.been.calledThrice;
+                expect(reporter).to.always.have.been.calledWithExactly('Trailing whitespace found.');
+            });
+
+            it('remains silent when no trailing whitespace is found', function() {
+                rule.check(context, true, 'foo\n');
+                rule.check(context, true, 'foo\r\n');
+                rule.check(context, true, '\r');
+                expect(reporter).to.not.have.been.called;
+            });
+
         });
 
-        it('remains silent when no trailing whitespace', function() {
-            rule.check(context, true, 'foo\n');
-            rule.check(context, true, 'foo\r\n');
-            rule.check(context, true, '\r');
-            expect(reporter).to.not.have.been.called;
+        it('fixes trailing whitespace by replacing it with nothing', function() {
+            var result;
+            result = rule.fix(true, 'foo \n');
+            expect(result).to.equal('foo\n');
+            result = rule.fix(true, 'foo	 \r\n');
+            expect(result).to.equal('foo\r\n');
+            result = rule.fix(true, '	 	\r');
+            expect(result).to.equal('\r');
         });
 
     });
 
     describe('false setting', function() {
 
-        it('ignores trailing whitespace', function() {
-            rule.check(context, false, 'foo \n');
-            rule.check(context, false, 'foo	 \r\n');
-            rule.check(context, false, '	 	\r');
-            expect(reporter).to.not.have.been.called;
+        describe('check command', function() {
+
+            it('ignores trailing whitespace', function() {
+                rule.check(context, false, 'foo \n');
+                rule.check(context, false, 'foo	 \r\n');
+                rule.check(context, false, '	 	\r');
+                expect(reporter).to.not.have.been.called;
+            });
+
+            it('ignores no trailing whitespace', function() {
+                rule.check(context, false, 'foo\n');
+                rule.check(context, false, 'foo\r\n');
+                rule.check(context, false, '\r');
+                expect(reporter).to.not.have.been.called;
+            });
+
         });
 
-        it('ignores no trailing whitespace', function() {
-            rule.check(context, false, 'foo\n');
-            rule.check(context, false, 'foo\r\n');
-            rule.check(context, false, '\r');
-            expect(reporter).to.not.have.been.called;
+        it('leaves trailing whitespace alone', function() {
+            var result;
+            result = rule.fix(false, 'foo \n');
+            expect(result).to.equal('foo \n');
+            result = rule.fix(false, 'foo	 \r\n');
+            expect(result).to.equal('foo	 \r\n');
+            result = rule.fix(false, '	 	\r');
+            expect(result).to.equal('	 	\r');
         });
 
     });
