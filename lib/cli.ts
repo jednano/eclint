d567cb27989a8c703464b2c96da9261ae6509833
刘祺 <gucong@gmail.com>
2017-03-04 23:10:58 +0800

    New features for CLI - Added: default value for options <files...> #46 - Added: exclude binary file #45 - Modified:  use `console.error` instead of `console.log` to output error message - Added: testcase for CLI

--- lib/cli.ts
+++ lib/cli.ts
@@ -1,22 +1,23 @@
 import _ = require('lodash');
-var tap = require('gulp-tap');
+const tap = require('gulp-tap');
 import File = require('vinyl');
 import vfs = require('vinyl-fs');
 import gutil = require('gulp-util');
 
 import eclint = require('./eclint');
 
-var cli = require('gitlike-cli');
-var pkg = require('../package');
-var reporter = require('gulp-reporter');
+const cli = require('gitlike-cli');
+const pkg = require('../package');
+const reporter = require('gulp-reporter');
+const filter = require('gulp-filter');
+const fileType = require('file-type');
+const binaryexts = require('binaryextensions');
 
 cli.on('error', err => {
-	console.log('');
-	console.log(gutil.colors.red('  ' + err.name + ':', err.message));
+	console.error('\n  ' + gutil.colors.red(err.toString()));
 	err.command.outputUsage();
 	err.command.outputCommands();
 	err.command.outputOptions();
-	console.log();
 	process.exit(1);
 });
 
@@ -34,26 +35,38 @@ function addSettings(cmd): void {
 	cmd.option('-m, --max_line_length <n>',      'Set to a whole number');
 }
 
+function excludeBinaryFile(file: File) {
+	const type = file && file.isBuffer() && fileType(file.contents);
+	return !(type && type.ext && binaryexts.indexOf(type.ext) >= 1);
+}
+
 interface CheckOptions extends eclint.Settings {
 	reporter?: (file: File, message: string) => void;
 }
 
-function handleNegativeGlobs(files: string[]): any {
-	return files.map(glob => {
-		return glob.replace(/^\[!\]/, '!');
-	});
+function handleNegativeGlobs(files?: string[]): string[] {
+	if (!files) {
+		return ['**/*', '!**/.git', '!**/.svn', '!**/.hg', '!**/.DS_Store', '!**/node_modules', '!**/bower_components'];
+	}
+	return files.filter(file => (
+		typeof file === 'string'
+	)).map(glob => (
+		glob.replace(/^\[!\]/, '!')
+	));
 }
 
-var check = cli.command('check <files>...');
+const check = cli.command('check [<files>...]');
 check.description('Validate that file(s) adhere to .editorconfig settings');
 addSettings(check);
 check.action((args: any, options: CheckOptions) => {
-	var stream = vfs.src(handleNegativeGlobs(args.files.filter(file => (typeof file === 'string'))), {
+	const stream = vfs.src(handleNegativeGlobs(args.files), {
 		stripBOM: false
 	})
+		.pipe(filter(excludeBinaryFile))
 		.pipe(eclint.check({
 			settings: _.pick(options, eclint.ruleNames),
 		})).pipe(reporter({
+			console: console.error,
 			filter: null,
 		}))
 		.on('error', (error) => {
@@ -72,14 +85,15 @@ interface FixOptions extends eclint.Settings {
 	dest?: string;
 }
 
-var fix = cli.command('fix <files>...');
+const fix = cli.command('fix [<files>...]');
 fix.description('Fix formatting errors that disobey .editorconfig settings');
 addSettings(fix);
 fix.option('-d, --dest <folder>', 'Destination folder to pipe source files');
 fix.action((args: any, options: FixOptions) => {
-	var stream = vfs.src(handleNegativeGlobs(args.files.filter(file => (typeof file === 'string'))), {
+	const stream = vfs.src(handleNegativeGlobs(args.files), {
 		stripBOM: false
 	})
+		.pipe(filter(excludeBinaryFile))
 		.pipe(eclint.fix({ settings: _.pick(options, eclint.ruleNames) }));
 	if (options.dest) {
 		return stream.pipe(vfs.dest(options.dest));
@@ -89,15 +103,16 @@ fix.action((args: any, options: FixOptions) => {
 	}));
 });
 
-var infer = cli.command('infer <files>...');
+const infer = cli.command('infer [<files>...]');
 infer.description('Infer .editorconfig settings from one or more files');
 infer.option('-s, --score', 'Shows the tallied score for each setting');
 infer.option('-i, --ini',   'Exports file as ini file type');
 infer.option('-r, --root',  'Adds root = true to your ini file, if any');
 infer.action((args: any, options: eclint.InferOptions) => {
-	return vfs.src(handleNegativeGlobs(args.files.filter(file => (typeof file === 'string'))), {
+	return vfs.src(handleNegativeGlobs(args.files), {
 		stripBOM: false
 	})
+		.pipe(filter(excludeBinaryFile))
 		.pipe(eclint.infer(options))
 		.pipe(tap((file: File) => {
 			console.log(file.contents + '');
