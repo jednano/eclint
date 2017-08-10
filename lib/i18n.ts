2e7f8b0fc7066c677b94445d38c0635198126905
刘祺 <gucong@gmail.com>
2017-08-10 15:52:36 +0800

    fix #81 (#99)

--- lib/i18n.ts
+++ lib/i18n.ts
@@ -1,16 +1,13 @@
-import fs = require('fs');
 import path = require('path');
 import osLocale = require('os-locale');
 
 const y18n = require('y18n')({
+	updateFiles: false,
 	directory: path.resolve(__dirname, '../locales')
 });
 
 const locale = osLocale.sync({ spawn: false });
 
-// bugfix for yargs/y18n#48
-if (fs.existsSync(path.join(y18n.directory, locale + '.json'))) {
-	y18n.setLocale(locale);
-}
+y18n.setLocale(locale);
 
 export = y18n.__.bind(y18n);
