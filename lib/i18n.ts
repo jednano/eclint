f2783129b8a1430927e96744bb3526216ec63a57
刘祺 <gucong@gmail.com>
2017-11-06 18:52:38 +0800

    fix y18n (#108)

--- lib/i18n.ts
+++ lib/i18n.ts
@@ -3,11 +3,8 @@ import osLocale = require('os-locale');
 
 const y18n = require('y18n')({
 	updateFiles: false,
-	directory: path.resolve(__dirname, '../locales')
+	directory: path.resolve(__dirname, '../locales'),
+	locale: osLocale.sync({ spawn: false })
 });
-
-const locale = osLocale.sync({ spawn: false });
-
-y18n.setLocale(locale);
 
 export = y18n.__.bind(y18n);
